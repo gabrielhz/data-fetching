624bc3551505ff6b10c9f31c4aa53b586a42a628
gabrielhz <gabrielramoscardoso@hotmail.com>
2024-04-16 14:29:12 -0300

    consuming github api and listing repositories

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+type Repository = {
+  full_name: string;
+  description: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+    const [repositories, setRepositories] = useState<Repository[]>([])
+    useEffect(() => {
+      fetch('https://api.github.com/users/gabrielhz/repos')
+      .then(response => response.json())
+      .then(data => {
+        setRepositories(data);
+      })
+    }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ul>
+      {repositories.map(repo => {
+        return(
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
